b2c68d5a014038a73db0320a277cfaaf5290eb92
Siarhei Lunski <loonskai@gmail.com>
2026-04-17 21:37:44 +0200

    Basic MIDI

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@
 use embassy_executor::Spawner;
 use embassy_time::Timer;
 use embassy_rp::{
-    bind_interrupts, gpio::{Level, Output, Pull}, peripherals::USB, pwm::{self, Config as PwmConfig}, usb::{Driver, InterruptHandler}
+    bind_interrupts, gpio::{Level, Output}, peripherals::USB, pwm::{Pwm, Config as PwmConfig}, usb::{Driver, InterruptHandler}
 };
-use embassy_usb::{Builder, Config as UsbConfig, UsbDevice, class::cdc_acm::{State}};
+use embassy_usb::{Config as UsbConfig, UsbDevice};
 use embassy_usb::class::midi::MidiClass;
 
 use static_cell::StaticCell;
@@ -23,8 +23,12 @@ async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
 
     let driver = Driver::new(p.USB, Irqs);
-    let mut config = UsbConfig::new(0x1209, 0x0001);
-    config.product = Some("Pico USB MIDI Clock");
+    let mut usb_config = UsbConfig::new(0x1209, 0x0001);
+    usb_config.product = Some("Pico USB MIDI Clock");
+    let mut pwm_config: PwmConfig = Default::default();
+    pwm_config.top = 2082;
+    pwm_config.divider = 1u8.into();
+    let mut pwm_out = Pwm::new_output_b(p.PWM_SLICE2, p.PIN_5, pwm_config.clone());
 
     let mut builder = {
         static CONFIG_DESCRIPTOR: StaticCell<[u8; 256]> = StaticCell::new();
@@ -33,7 +37,7 @@ async fn main(spawner: Spawner) {
 
         let builder = embassy_usb::Builder::new(
             driver,
-            config,
+            usb_config,
             CONFIG_DESCRIPTOR.init([0; 256]),
             BOS_DESCRIPTOR.init([0; 256]),
             &mut [], // no msos descriptors
@@ -50,6 +54,7 @@ async fn main(spawner: Spawner) {
     spawner.spawn(usb_task(usb)).unwrap();
     let mut pulse = Output::new(p.PIN_15, Level::Low);
     let mut led = Output::new(p.PIN_14, Level::Low);
+    let mut note_led = Output::new(p.PIN_13, Level::Low);
     let mut tick_count: u32 = 0;
 
     loop { 
@@ -58,15 +63,46 @@ async fn main(spawner: Spawner) {
         midi.wait_connection().await;
         if let Ok(n) = midi.read_packet(&mut buf).await {
             for chunk in buf[..n].chunks(4) {
-                if chunk.len() >= 2 && chunk[1] == 0xF8 {
-                    tick_count += 1;
-                    // 6 ticks = sixteenth note
-                    if tick_count % 6 == 0 {
-                        pulse.set_low();
-                        led.set_high();
-                        Timer::after_millis(5).await;
-                        pulse.set_high();
-                        led.set_low();
+                defmt::info!("MIDI: {:02x} {:02x} {:02x} {:02x}", chunk[0], chunk[1], chunk[2], chunk[3]);
+                if chunk.len() >= 2 {
+                    match chunk[1] & 0xF0 {
+                        // clock
+                        0xF0 => {
+                            if chunk[1] == 0xF8 {
+                                tick_count += 1;
+                                // 6 ticks = sixteenth note
+                                if tick_count % 24 == 0 {
+                                    pulse.set_low();
+                                    led.set_high();
+                                    Timer::after_millis(5).await;
+                                    pulse.set_high();
+                                    led.set_low();
+                                }
+                            }
+                        },
+                        // note on
+                        0x90 => {
+                            match chunk {
+                                [_, _, note, velocity] => {
+                                    defmt::info!("Note ON: note={} vel={} duty={}", 
+                note, velocity, (*note as u32 * 52));
+                                    if *velocity == 0 {
+                                        note_led.set_low();
+                                    } else {
+                                        note_led.set_high();
+                                        let duty = ((*note as u32).saturating_sub(36) * 52) as u16;
+                                        pwm_config.compare_b = duty;
+                                        pwm_out.set_config(&pwm_config);
+                                    }
+                                },
+                                _ => {}
+                            }
+                        },
+                        // note off
+                        0x80 => {
+                            note_led.set_low();
+                        },
+                        _ => {}
                     }
                 }
             }
